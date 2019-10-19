93fa0c8071cdfea9c788979d16014cb305d8ddaf
CorollaD <1976007504@qq.com>
2019-10-20 00:47:59 +0800

    third commit

--- test_my_except.py
+++ test_my_except.py
@@ -1,26 +1,53 @@
 
 
-class MyException(Exception):
-    """ 我的自定义异常"""
-    pass
+class ApiException(Exception):
+    """ 我的自定义异常 """
+    err_code = ''
+    err_msg = ''
 
-
-class InvalidCtrlExec(Exception):
-    """ 当参数不合法时触发"""
-    def __init__(self, err_code, err_msg):
+    def __init__(self, err_code=None, err_msg=None):
         """
 
         :param err_code:
         :param err_msg:
         """
-        self.err_code = err_code
-        self.err_msg = err_msg
+        self.err_code = self.err_code if self.err_code else err_code
+        self.err_msg = self.err_msg if self.err_msg else err_msg
 
     def __str__(self):
         return 'Error: {0} - {1}'.format(self.err_code, self.err_msg)
 
 
+class InvalidCtrlExce(ApiException):
+    """ 当参数不合法时触发
+    40001 invalid credential  不合法的调用凭证
+    """
+    err_code = '400001'
+    err_msg = '不合法的调用凭证'
 
-def test_exec(num1, num2):
-    """ 除法的实现"""
-    if isinstance()
+
+class BadPramsException(ApiException):
+    """ 参数不正确 """
+    err_code = '40002'
+    err_msg = '两个参数必须是整数'
+
+
+def divide(num1, num2):
+    """ 除法的实现 """
+    # 两个数必须为整数
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        raise BadPramsException()
+    # 除数不能为0
+    if num2 == 0:
+        raise ApiException('400000', '除数不能为0')
+    return num1 / num2
+
+
+if __name__ == '__main__':
+
+    try:
+        rest = divide(5, 's')
+        print(rest)
+    except ApiException as err:
+        print('出错了')
+        print(err)
---
+++ venv/test_my_except1.py
@@ -0,0 +1,24 @@
+
+
+class ShortInputException(Exception):
+    """ 自定义异常类"""
+    def __init__(self, length, atleast):
+        self.length = length
+        self.atleast = atleast
+
+def main():
+    try:
+        text = 'abc'
+        if len(text) < 5:
+            raise ShortInputException(len(text), 5)
+    except ShortInputException as result:
+        print("ShortInputException:输入的长度为：{0}，" " 长度至少应该是：{1}".format(result.length, result.atleast))
+
+    else:
+        print("没有异常发生")
+    finally:
+        print("输入的字符串是{}".format(text))
+
+
+if __name__ == '__main__':
+    main()
